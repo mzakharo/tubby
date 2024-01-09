4e3281ad3e4604fa3ae4cb6d0a8fc9cfe8922f33
Mikhail Zakharov <mzakharo@gmail.com>
2024-01-09 07:47:39 -0500

    cleanup sensor attributes

--- light_sleep.h
+++ light_sleep.h
@@ -4,31 +4,36 @@
 
 class LightSleep : public PollingComponent, public Sensor {
  public:
+ int duration;
   // constructor
-  LightSleep() : PollingComponent(15000) {}
+  LightSleep(int dur) : PollingComponent(15000) {
+	 duration = dur; 
+	  
+  }
 
   float get_setup_priority() const override { return esphome::setup_priority::HARDWARE; }
 
   void setup() override {
     // This will be called by App.setup()
+	
 	//pinMode(LED_BUILTIN, OUTPUT);
 	//digitalWrite(LED_BUILTIN, HIGH);
     gpio_set_direction(LED_BUILTIN, GPIO_MODE_OUTPUT);
-    gpio_set_level(LED_BUILTIN, 1);
-
-	
+    gpio_set_level(LED_BUILTIN, 1);	
 	
 	//pinMode(LDO2, OUTPUT);
 	//digitalWrite(LDO2, HIGH);
     gpio_set_direction(LDO2, GPIO_MODE_OUTPUT);
     gpio_set_level(LDO2, 1);
 
-	// wait for a second   
-	//esp_sleep_enable_timer_wakeup(3000000); //3 seconds
-    esp_sleep_enable_timer_wakeup(900000000); //15 minutes
-	int ret = esp_light_sleep_start();
+	//settle sensors
+	esp_sleep_enable_timer_wakeup(duration);
+	
+	esp_light_sleep_start();
+	
 	//digitalWrite(LED_BUILTIN, LOW);
 	gpio_set_level(LED_BUILTIN, 0);
+	
   }
   void update() override {
     // This will be called every "update_interval" milliseconds.	
